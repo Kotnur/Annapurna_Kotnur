a24e70a90429edab3ccfef15741cc6bacd13cc77
Kotnur <111430001+Kotnur@users.noreply.github.com>
2023-03-30 20:40:09 +0530

    Create Factorial.java

---
+++ Factorial.java
@@ -0,0 +1,10 @@
+public class Factorial{
+	public static void main(String[] args){
+	int i, x =1;
+	int y =10;
+	for(i=1;i<=y;i++){
+	y=y*i;
+	}
+	System.out.println("Factorial of 1 to 10 is"+y);
+	}
+	}
